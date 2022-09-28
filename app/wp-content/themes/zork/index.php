976d2893502e6036b3ba23abc191286ad5ff854f
Rich Cookson <hellorich@frobitz.com>
2022-09-28 15:21:53 +0100

    Set up basic home page

--- app/wp-content/themes/zork/index.php
+++ app/wp-content/themes/zork/index.php
@@ -1,2 +1,19 @@
 <?php
-  wp_redirect(site_url('wp-admin'));
+/**
+ * Zork Wordpress theme
+ */
+?>
+
+<!DOCTYPE html>
+<html <?php language_attributes(); ?>>
+  <head>
+    <meta charset="<?php bloginfo( 'charset' ); ?>">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0" >
+    <title><?php echo get_bloginfo( 'name' ); ?></title>
+    <link href="<?php echo get_template_directory_uri(); ?>/style.css" rel="stylesheet">
+  </head>
+  
+  <body>
+    <img alt="<?php echo get_bloginfo( 'name' ); ?>" class="logo" src="<?php echo get_template_directory_uri(); ?>/images/logo.svg">
+  </body>
+</html>
