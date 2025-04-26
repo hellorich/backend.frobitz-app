12586924c52715da7637e5b145f3ace9bfadef80
Rich <hellorich@frobitz.com>
2025-04-26 14:45:43 +0100

    Remove old plugins to replace with Faust.js

--- app/wp-content/plugins/wp-graphql/build/app.asset.php
+++
@@ -1,1 +0,0 @@
-<?php return array('dependencies' => array('react', 'react-dom', 'wp-element', 'wp-hooks'), 'version' => 'e4db36ac3a701438f12c');
--- app/wp-content/plugins/wp-graphql/build/index.asset.php
+++
@@ -1,1 +0,0 @@
-<?php return array('dependencies' => array('wp-element', 'wp-hooks'), 'version' => '4e3fb6c1c0df33a85f55');
